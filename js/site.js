852defb5fb0a18cb6845d48e14428f75ceeebe7b
Sagar <bp.sagar@gmail.com>
2025-03-06 00:34:16 +0800

    Refactor CSS and HTML to remove preloader and unused font imports for improved performance

--- js/site.js
+++ js/site.js
@@ -51,7 +51,6 @@ $(document).ready(function () {
 /* Preloader and animations */
 $(window).load(function () { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(350).css({'overflow-y': 'visible'});
 
 	/* WOW Elements */
